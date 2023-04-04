63da4c614731901f726b587e74e1f2fdbbd40ebf
Neillcllghn <neillcllghn@outlook.com>
2023-04-04 19:13:06 +0000

    Login page update with CSS etc.

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
     path('create/', views.BookingCreate.as_view(), name='create_bookings'),
-    path('', views.index, name='home'),
 ]
