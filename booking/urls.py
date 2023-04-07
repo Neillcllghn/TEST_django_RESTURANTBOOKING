de1a2cc6972c94804dbd9f69840a564b20bed6b0
Neillcllghn <neillcllghn@outlook.com>
2023-04-07 12:52:25 +0000

    Adding media folder with image to Static folders

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
     path('create/', views.BookingCreate.as_view(), name='create_bookings'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
