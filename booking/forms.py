8016821763c5c56293b25ecc9669197230341849
Neillcllghn <neillcllghn@outlook.com>
2023-04-07 12:56:19 +0000

    Formating the BookingForm template

--- booking/forms.py
+++ booking/forms.py
@@ -7,11 +7,13 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ('email', 'group_size', 'day', 'time',)
         labels = {
-            'day': 'Select reservation date', 
+            'day': 'Select reservation date',
             'time': 'Select time of reservation'
         }
 
         widgets = {
-            'day': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'time': forms.TimeInput(attrs={'format':'%H', 'class': 'form-control', 'type': 'time'})
+            'day': forms.DateInput(attrs={'class': 'form-control',
+                                   'type': 'date'}),
+            'time': forms.TimeInput(attrs={'format': '%H',
+                                    'class': 'form-control', 'type': 'time'})
         }
