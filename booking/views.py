0f3d542b4c8c3abacafe1e6d1a3a4e57736d436b
Neillcllghn <neillcllghn@outlook.com>
2023-04-08 11:19:45 +0000

    Enbaling user to create and post a booking and view it

--- booking/views.py
+++ booking/views.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from .models import Booking
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 from .forms import BookingForm
 
 
@@ -16,24 +16,31 @@ class BookingList(generic.ListView):
     model = Booking
     template_name = 'bookings.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['bookings'] = self.request.user.booking_set.all()
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['bookings'] = self.request.user.booking_set.all()
+    #     return context
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
 
 
-class BookingCreate(FormView):
+class BookingCreate(CreateView):
     model = Booking
     template_name = 'create_bookings.html'
     form_class = BookingForm
-    success_url = 'booking/'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 # work in progress
-    def register(request):
-        form_class = BookingForm(data=request.POST)
-        if request.method == 'POST':
-            if form_class.is_valid():
-                form_class.save()
-                return render(request, 'bookings.html', {'form': form})
-            else:
-                form_class = BookingForm()
+    # def register(request):
+    #     form_class = BookingForm(data=request.POST)
+    #     if request.method == 'POST':
+    #         if form_class.is_valid():
+    #             form_class.save()
+    #             return render(request, 'bookings.html', {'form': form})
+    #         else:
+    #             form_class = BookingForm()
