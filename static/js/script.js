47230d6dcf235976fe9a783014fc04cf7a256fbd
Neillcllghn <neillcllghn@outlook.com>
2023-04-10 11:56:16 +0000

    ADDING Model to delete button

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,10 @@
 let sidenav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sidenav);
+
+let modals = document.querySelectorAll('.modal');
+M.Modal.init(modals);
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
