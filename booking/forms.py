ae18829462458a9126b57e7d74930d39718cc63a
Neillcllghn <neillcllghn@outlook.com>
2023-04-16 15:58:09 +0000

    Added in Validation errors to form.py for number of people and day

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,6 @@
 from .models import Booking, TIME_CHOICES
 from django import forms
+from datetime import date
 from django.core.exceptions import ValidationError
 
 
@@ -18,3 +19,17 @@ class BookingForm(forms.ModelForm):
             'time': forms.TimeInput(attrs={'format': '%H',
                                     'class': 'form-control', 'type': 'time'})
         }
+
+    def clean_group_size(self):
+        group_size = self.cleaned_data.get('group_size')
+        if int(group_size) < 1 or int(group_size) > 6:
+            raise ValidationError(f"You selected {group_size}, "
+                                  "please select between 1 and 6 guests")
+        return group_size
+
+    def clean_day(self):
+        day = self.cleaned_data.get('day')
+        if day < date.today():
+            raise ValidationError("You must select today's date "
+                                  "or a future date")
+        return day
