8b7e1334ff997889c1cb0b0556bdc9c48e67d8c0
Neillcllghn <neillcllghn@outlook.com>
2023-04-18 16:03:52 +0000

    Updated delete function with Modal popup

--- static/js/script.js
+++ static/js/script.js
@@ -5,21 +5,5 @@ M.Sidenav.init(sidenav);
 let collapsible = document.querySelectorAll('.collapsible');
 M.Collapsible.init(collapsible);
 
-// Get the modal
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+let modal = document.querySelectorAll('.modal');
+M.Modal.init(modal);
