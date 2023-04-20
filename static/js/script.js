a97c26336736d9a461bd8ba6f285b31aa1204df1
Neillcllghn <neillcllghn@outlook.com>
2023-04-20 17:36:43 +0000

    Fade out messages after 2 -3 secs

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,11 @@ M.Collapsible.init(collapsible);
 
 let modal = document.querySelectorAll('.modal');
 M.Modal.init(modal);
+
+var messages = document.getElementsByClassName("messages");
+
+setTimeout(function(){
+   if (messages && messages.length) {
+       messages[0].classList.add('hide');
+   }
+}, 3000);
